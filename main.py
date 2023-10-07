3d3452487947fa0e1714257911a35f7a78697063
Cristhian <ccdelgadop@gmail.com>
2023-10-07 17:36:09 -0500

    remove cron library

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from debugger import initialize_flask_server_debugger_if_needed
 
 from flask import Flask, request
 from multiprocessing import Process
-from flask_crontab import Crontab
 import requests
 from datetime import datetime
 
@@ -23,21 +22,8 @@ from config import config
 # debbugProcess.join()
 
 app = Flask(__name__)
-crontab = Crontab(app)
 supplies_service = Supplies_Service()
 
-@crontab.job(
-    minute="40",
-    hour="05" # middle nigth in Colombia
-)
-def my_scheduled_job():
-    """Cron job to update products"""
-    now = datetime.now()
-    time = now.strftime('%H:%M:%S')
-    print("api call: {time}")
-    r = requests.get(f'http://{config["IP_SERVER"]}:5000/sync/cris')
-    # r = requests.get(f'http://localhost:5000') # test cron
-    
 
 @app.route('/', methods=['GET'])
 def test():
@@ -46,13 +32,13 @@ def test():
     return "ok"
 
 
-
 @app.route('/sync/<key>', methods=['GET'])
 def test_call(key):
     user_ip = request.remote_addr
     print('request.args')
-    if key == 'cris' :
+    if key == 'cris':
         response = supplies_service.get_products(MARPICO)
-        if response: return 'ok'
+        if response:
+            return 'ok'
         return f'Im runing {user_ip}'
     return 'You dont have access to this site'
